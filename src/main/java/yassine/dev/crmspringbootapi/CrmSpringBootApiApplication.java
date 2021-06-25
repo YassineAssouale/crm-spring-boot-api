29297bc1a07f771f2ae63884604758fdf0c82e7b
Yassine <yassine.assouale@gmail.com>
2021-06-25 10:19:22 +0200

    Creating Spring boot application && configuring pom.xml, gitignore files

---
+++ src/main/java/yassine/dev/crmspringbootapi/CrmSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package yassine.dev.crmspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yassine/dev/crmspringbootapi/CrmSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yassine.dev.crmspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
